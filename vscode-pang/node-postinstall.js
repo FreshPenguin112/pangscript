39732db781752f12952b585f186298d4640964fa
FreshPenguin112 <freshpenguin112@gmail.com>
2026-04-28 17:08:56 -0500

    foo

--- vscode-pang/node-postinstall.js
+++
@@ -1,5 +0,0 @@
-// Postinstall helper: attempt to install lightweight deps locally if missing.
-// This is a no-op placeholder so `npm install` doesn't fail in environments without network.
-try {
-  // noop
-} catch (e) {}
